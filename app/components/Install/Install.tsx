42459ac99995ccae4bd68feede6698555b30828c
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-05-23 10:24:01 +0900

    update

--- app/components/Install/Install.tsx
+++ app/components/Install/Install.tsx
@@ -31,7 +31,11 @@ const Install = () => {
   };
 
   return (
-    <button disabled={!ready} onClick={handleClick}>
+    <button
+      className="bg-blue-500 hover:bg-blue-700 text-white text-sm font-bold py-1 px-2 rounded mr-2 disabled:bg-gray-400"
+      disabled={!ready}
+      onClick={handleClick}
+    >
       インストール
     </button>
   );
