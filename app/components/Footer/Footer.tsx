eb943e77da49197b5f308c962ae24afbbd641698
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-06-18 11:02:15 +0900

    update

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,13 +1,14 @@
 import { BsQrCode } from 'react-icons/bs';
 import { BsBell } from 'react-icons/bs';
 import { IoMdHome } from 'react-icons/io';
+import { FaPenAlt } from 'react-icons/fa';
 
 import Link from 'next/link';
 
 const Footer = () => {
   return (
     <div className="sticky bottom-0 left-0 z-50 w-full h-full bg-white border-t border-gray-200 dark:bg-gray-300 dark:border-gray-200">
-      <div className="grid h-full max-w-lg grid-cols-3 mx-auto font-medium pb-4 pt-2">
+      <div className="grid h-full max-w-lg grid-cols-4 mx-auto font-medium pb-4 pt-2">
         <Link
           href="/"
           className="inline-flex flex-col items-center justify-center group"
@@ -34,6 +35,16 @@ const Footer = () => {
           <BsBell className="size-5 text-gray-500 group-hover:text-blue-600 " />
           <span className="text-[0.5rem] text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">
             通知
+          </span>
+        </Link>
+
+        <Link
+          href="/signature"
+          className="inline-flex flex-col items-center justify-center group"
+        >
+          <FaPenAlt className="size-5 text-gray-500 group-hover:text-blue-600 " />
+          <span className="text-[0.5rem] text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">
+            署名
           </span>
         </Link>
       </div>
