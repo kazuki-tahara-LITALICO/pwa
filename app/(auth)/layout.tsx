248afccafb8b8999a0e855b01bbdff1fc9589d45
田原 一樹 <ssp0727.lnc@gmail.com>
2024-06-20 13:48:37 +0900

    update

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,5 @@
 import { ReactNode } from "react";
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
-  return (
-    <div className="flex h-screen flex-col items-center justify-start px-5 bg-white">
-      {children}
-    </div>
-  );
+  return <>{children}</>;
 }
--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,23 +1,62 @@
 import React from "react";
-import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 const LoginPage = () => {
   return (
-    <div className="mt-40 w-full">
-      <h1 className="text-3xl text-center font-normal">アプリアプリ</h1>
-      <div className="space-y-4 mt-10">
-        <p className="text-center font-normal text-sm">
-          サンプルサンプルご利用の方
+    <div className="w-screen pt-8 pb-7 px-4">
+      <div className="space-y-2">
+        <h1 className="text-2xl text-left font-normal">アカウントアカウント</h1>
+        <p className="flex flex-row gap-2 text-sm items-end">
+          サンプル<span className="text-xs text-red-600">必須</span>
         </p>
-        <Button>新規登録</Button>
-      </div>
-      <div className="space-y-4 mt-6">
-        <p className="text-center font-normal text-sm">
-          サンプルサンプル・サンプルサンプル
-        </p>
-        <Button variant="outline">ログイン</Button>
+        <SelectComponent
+          placeholder="選択してください"
+          valueObjectArray={sampleValueObjectArray}
+        />
       </div>
     </div>
+  );
+};
+
+// ------------------------- Select ---------------------
+const sampleValueObjectArray = [
+  { value: "sample1", displayName: "Sample1" },
+  { value: "sample1", displayName: "Sample1" },
+  { value: "sample1", displayName: "Sample1" },
+];
+
+type SelectComponent = {
+  placeholder?: string;
+  valueObjectArray: { value: string; displayName: string }[];
+};
+
+const SelectComponent = ({
+  placeholder,
+  valueObjectArray,
+}: SelectComponent) => {
+  return (
+    <Select>
+      <SelectTrigger className="w-full border-black py-3 px-4 mt-2">
+        <SelectValue className="text-base" placeholder={placeholder} />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          {valueObjectArray.map(({ value, displayName }, i) => (
+            <SelectItem key={i} value={value}>
+              {displayName}
+            </SelectItem>
+          ))}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
   );
 };
 
